3e6f1f114cbdd5fc7449b62269dd2c50ddda1c8e
Jhon-Jordan <jairojordanherrera@gmail.com>
2025-05-07 09:36:57 -0500

    Update clase.color.py

--- clase.color.py
+++ clase.color.py
@@ -1,7 +1,1 @@
-class Color:
-    def __init__(self, color):
-        self._color = color
-    def get_color(self, color):
-        return self._color
-    def set_color(self, color):
-        return self._color
+
